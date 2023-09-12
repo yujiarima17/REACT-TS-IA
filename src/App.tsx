58019d1c2660eb9e4977fc8bf103f517090cf8ba
Yuji Arima <arimayuji2@gmail.com>
2023-09-12 15:13:00 -0300

    header layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 import { Button } from "@/components/ui/button";
-
+import { Github } from "lucide-react";
+import { Separator } from "@/components/ui/separator";
 export function App() {
   return (
-    <div>
-      <Button>Click me</Button>
+    <div className="min-h-screen flex flex-col">
+      <div className="px-6 py-3 flex items-center justify-between border-b">
+        <h1 className="text xl font-bold">upload.ai</h1>
+        <div className="flex items-center gap-3">
+          <span className="text-sm text-muted-foreground">
+            Developed in NLW IA
+          </span>
+          <Separator orientation="vertical" className="h-6" />
+
+          <Button variant="outline">
+            <Github className="w-4 h-4 mr-2" />
+          </Button>
+        </div>
+      </div>
+      
     </div>
   );
 }
